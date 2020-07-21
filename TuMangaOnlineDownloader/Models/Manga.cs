62f74edaede06bbd2c3a7a7973907b4a9fd8dbe9
Julio Olivares <julioolivares90@hotmail.com>
2020-07-20 19:38:23 -0600

    add models and Class scraper

---
+++ TuMangaOnlineDownloader/Models/Manga.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuMangaOnlineDownloader.Models
+{
+    public class Manga
+    {
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Demografia { get; set; }
+
+        public double Score { get; set; }
+
+        public string Description { get; set; }
+
+        public string []Generos { get; set; }
+
+        public string  Estado { get; set; }
+
+        public string[] Capitulos { get; set; }
+    }
+}
---
+++ TuMangaOnlineDownloader/Scraper/ScraperManga.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuMangaOnlineDownloader.Scraper
+{
+    public class ScraperManga
+    {
+
+    }
+}
